25641b56a7cd525c914aaf00282851946fe652d3
Christian Koch Echeverria <s0566264@htw-berlin.de>
2026-02-24 18:03:26 +0100

    Clean up

--- backend/src/utils/chunking.ts
+++ backend/src/utils/chunking.ts
@@ -3,17 +3,6 @@ export interface TextSplitConfig {
 	chunkOverlap: number;
 }
 // Sliding window algoirithm for chunking
-/**
- * const chunkSize = 5
- * const chunkOverlap = 0
- * const stride =
- * stride = windowSize - Olverap
- * const textSize = 10
- * [].size 5 [].size
- * my name is chris codes
- * codes who loves coding
- *
- */
 export function chunkText(text: string, config: TextSplitConfig) {
 	const { chunkSize, chunkOverlap } = config;
 	if (chunkSize <= chunkOverlap) {
@@ -34,10 +23,3 @@ export function chunkText(text: string, config: TextSplitConfig) {
 
 	return chunks;
 }
-// Debug testing
-// const text = "I am chris codes and I love to code <3";
-// const config = {
-// 	chunkSize: 5,
-// 	chunkOverlap: 2,
-// };
-// console.log(chunkText(text, config));
